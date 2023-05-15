075f20cdd3398fae228ecac5528ca52b8046452b
Yash Patidar <yashpatidar964@gmail.com>
2023-05-16 01:11:46 +0530

    Add - added Products details in Db

--- plantkart/src/backend/db/products.js
+++ plantkart/src/backend/db/products.js
@@ -8,23 +8,122 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    name: "Areca Palm",
+    price: "2499",
+    rating: 5.0,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/vB3zSQxZ/Areca-palm.jpg"
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    name: "Broken Heart",
+    price: "179",
+    rating: 4.8,
+    categoryName: "indoor",
+    image_link:"https://i.postimg.cc/mD9s45RM/Broken-Heart.jpg"
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    name: "Fittonia Green",
+    price: "190",
+    rating: 4.8,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/L4qT9FrG/Fitonia-green.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Monstera Deliciosa",
+    price: "1169",
+    rating: 4.5,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/MpGXwqLQ/Monstera-Deliciosa.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Peperomia Green",
+    price: "200",
+    rating: 4.7,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/D0y3GyLQ/Peperomia-Green.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Peperomia Green",
+    price: "250",
+    rating: 4.7,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/X74kwBZ6/Golden-snake-Plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Jade Plant",
+    price: "160",
+    rating: 4.7,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/hjCcpdSZ/Jade-Plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Snake Plant",
+    price: "500",
+    rating: 5.0,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/rm5Yp6H1/snake-plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Rubber Plant",
+    price: "299",
+    rating: 4.3,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/rmbFqrv0/Rubber-Plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Red Plant",
+    price: "349",
+    rating: 4.8,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/gJ4thyYj/Red-plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Bamboo Palm Plant",
+    price: "269",
+    rating: 5.0,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/KjRfgdqp/Bamboo-palm-plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Wandering Jew",
+    price: "999",
+    rating: 4.9,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/xCF3tRq4/Wandering-jew.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Peacock Plant",
+    price: "599",
+    rating: 4.8,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/6pWJsWMx/Peacock-Plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Snake Plant Green",
+    price: "199",
+    rating: 4.7,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/5NZYprgz/Snake-Plant-Green.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Ficus Plant",
+    price: "1499",
+    rating: 4.0,
+    categoryName: "indoor",
+    image_link: "https://i.postimg.cc/ZKVZ5vB5/Ficus.jpg"
   },
 ];
