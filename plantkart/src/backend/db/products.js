de955c55c0b825356035012d39cdcce4606e4c41
Yash Patidar <yashpatidar964@gmail.com>
2023-05-16 11:51:55 +0530

    Add - Added more products and categories

--- plantkart/src/backend/db/products.js
+++ plantkart/src/backend/db/products.js
@@ -126,4 +126,132 @@ export const products = [
     categoryName: "indoor",
     image_link: "https://i.postimg.cc/ZKVZ5vB5/Ficus.jpg"
   },
+  {
+    _id: uuid(),
+    name: "Kesar Mango Plant",
+    price: "949",
+    rating: 3.5,
+    categoryName: "fruit",
+    image_link: "https://i.postimg.cc/HxTWLz16/Kesar-mango-plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Chiku Plant",
+    price: "999",
+    rating: 4.2,
+    categoryName: "fruit",
+    image_link: "https://i.postimg.cc/qRmrvdL7/Chiku-plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Anthurium Million Flowers",
+    price: "599",
+    rating: 4.8,
+    categoryName: "flower",
+    image_link: "https://i.postimg.cc/qRmrvdL7/Chiku-plant.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Poinsettia Plant",
+    price: "799",
+    rating: 4.7,
+    categoryName: "flower",
+    image_link: "https://i.postimg.cc/HnK4XJLJ/Poinsettia.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Rugmini Plant Red",
+    price: "699",
+    rating: 4.8,
+    categoryName: "flower",
+    image_link: "https://i.postimg.cc/Y0JYjJXY/Rugmini-plant-red.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "The Dekstop Bundle",
+    price: "1399",
+    rating: 5.0,
+    categoryName: "bundle",
+    image_link: "https://i.postimg.cc/ht7zfyx1/The-Dekstop-bundle.png"
+  },
+  {
+    _id: uuid(),
+    name: "The Indoor Plant Bundle",
+    price: "1299",
+    rating: 4.9,
+    categoryName: "bundle",
+    image_link: "https://i.postimg.cc/y8Bx811s/The-indoor-plant-bundle.png"
+  },
+  {
+    _id: uuid(),
+    name: "The Low Maintainance Bundle",
+    price: "1299",
+    rating: 4.8,
+    categoryName: "bundle",
+    image_link: "https://i.postimg.cc/kXTzBw9G/The-low-maintainance-bundle.png"
+  },
+  {
+    _id: uuid(),
+    name: "The Sleep Better Bundle",
+    price: "1299",
+    rating: 4.7,
+    categoryName: "bundle",
+    image_link: "https://i.postimg.cc/zBM3zTBc/The-sleep-better-bundle.png"
+  },
+  {
+    _id: uuid(),
+    name: "The Sun Loving Bundle",
+    price: "1499",
+    rating: 5.0,
+    categoryName: "bundle",
+    image_link: "https://i.postimg.cc/xC73bsgG/The-sun-loving-succulent-bundle.png"
+  },
+  {
+    _id: uuid(),
+    name: "The Pack Of 4",
+    price: "1199",
+    rating: 5.0,
+    categoryName: "pots",
+    image_link: "https://i.postimg.cc/4NHFwc9N/Pack-of-4.png"
+  },
+  {
+    _id: uuid(),
+    name: "The Pack Of 4",
+    price: "1199",
+    rating: 5.0,
+    categoryName: "pots",
+    image_link: "https://i.postimg.cc/4NHFwc9N/Pack-of-4.png"
+  },
+  {
+    _id: uuid(),
+    name: "Football Ceramic Plant",
+    price: "399",
+    rating: 4.7,
+    categoryName: "pots",
+    image_link: "https://i.postimg.cc/XJ1Jxmyh/Football-ceramic.png"
+  },
+  {
+    _id: uuid(),
+    name: "Wooden Planter",
+    price: "899",
+    rating: 5.0,
+    categoryName: "pots",
+    image_link: "https://i.postimg.cc/ncXXCFQD/Wooden-Planter.jpg"
+  },
+  {
+    _id: uuid(),
+    name: "Self Watering",
+    price: "1299",
+    rating: 5.0,
+    categoryName: "pots",
+    image_link: "https://i.postimg.cc/brcqrPXQ/Self-watering.png"
+  },
+  {
+    _id: uuid(),
+    name: "Plastic Pots",
+    price: "699",
+    rating: 5.0,
+    categoryName: "pots",
+    image_link: "https://i.postimg.cc/kGnhfYK0/Plastic-pots.png"
+  },
 ];
