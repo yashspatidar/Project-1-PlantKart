a951ae91203441ffa3c5482a6d1f22a82424d056
Yash Patidar <yashpatidar964@gmail.com>
2023-05-21 01:48:38 +0530

    Style- added style in the cart

--- plantkart/src/Pages/Cart/Cart.jsx
+++ plantkart/src/Pages/Cart/Cart.jsx
@@ -1,36 +1,32 @@
-import { productDemo } from "../productDemo"
-// import {CartStyle } from "src/Pages/Cart"
-// import {CartStyle} from "../Cart/CartStyle"
-import "../Cart/CartStle.css"
-export const Cart = ()=>{
-    return(
-        
-        <div className="cartContainer">
-            <p className="cart-first">My Cart</p>
-            <div>
-            {productDemo.map((item)=>(
-                <>
-                <div>
-                    <img src={item.image_link} alt="cartImage"/>
-                </div>
-                <div>
-                    <p>{item.name}</p>
-                    <p>{item.price}</p>
-                    <button>Remove from Cart</button>
-                    <button>Add to Wishlist</button>
-                </div>
-                </>
-            ))}
-            </div>
-            {/* <div>
-                <p>PRICE DETAILS</p>
-                <div>
-                <p>Price({productDemo?.length} item)</p>
-                <p></p>
-                </div>
-                
+import { productDemo } from "../productDemo";
+import "../../Pages/Cart/CartStyle.css";
 
-            </div> */}
+export const Cart = () => {
+  return (
+    <div className="cartContainer">
+      <p className="cart-first">My Cart</p>
+      <div className="cart-card-container">
+        <div>
+          {productDemo.map((item) => (
+            <div className="cart-card">
+              <img src={item.image_link} alt="cartImage" />
+              <div className="cart-card-first">
+                <p>{item.name}</p>
+                <p className="cartPrice">{item.price}</p>
+                <button className="cartButtons">Remove from Cart</button>
+                <button className="cartButtons">Add to Wishlist</button>
+              </div>
+            </div>
+          ))}
         </div>
-    )
-}
+        <div>
+          <p>PRICE DETAILS</p>
+          <div>
+            <p>Price({productDemo?.length} item)</p>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
